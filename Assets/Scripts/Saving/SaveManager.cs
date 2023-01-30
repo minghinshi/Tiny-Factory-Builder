cf17216880b130670d2e1e35baeb8d669f8cd492
minghinshi <mhshi9768@gmail.com>
2023-01-30 21:38:09 +0800

    Fixed saving bug

--- Assets/Scripts/Saving/SaveManager.cs
+++ Assets/Scripts/Saving/SaveManager.cs
@@ -23,6 +23,7 @@ public class SaveManager : MonoBehaviour
         JsonSerializerSettings settings = new();
         settings.Converters.Add(new ScriptableObjectConverter());
         settings.Converters.Add(new Vector2IntConverter());
+        settings.NullValueHandling = NullValueHandling.Ignore;
         return settings;
     }
 
